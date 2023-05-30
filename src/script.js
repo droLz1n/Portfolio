edbfea1e544c9f61738d0e8eececdba449d09e13
Bowie <marketing@letscode.com.br>
2023-05-29 21:01:28 -0300

    nova aba projetos

---
+++ src/script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    looFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
